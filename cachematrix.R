0f34d462101d5cc5c7da9b0b4c3aadd016afa07c
ishajaiswal1912 <54535128+ishajaiswal1912@users.noreply.github.com>
2020-05-27 22:08:22 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -10,9 +10,7 @@ makeCacheMatrix <- function(x = matrix()) {
   list(set = set, get = get, 
        setInverse = setInverse, 
        getInverse = getInverse)
-}
 
-##Please include your own comment to explain your code (Required in Rubric)
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
